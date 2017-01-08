0e53528fbfb8fe52892af4c84f90ef3ab14f5339
Saar Katz <kats.saar@gmail.com>
2017-01-08 12:54:59 +0200

    Fixed spGerRGBHist. Added main with small test.

--- sp_image_proc_util.cpp
+++ sp_image_proc_util.cpp
@@ -15,26 +15,22 @@ extern "C" {
 using namespace cv;
 
 //helpermethod
-double* matrixToArray(Mat* m, int size) {
-  double* array = (double*)malloc(size * sizeof(double));
-  for (int i = 0; i < size; i++) {
-    array[i] = m->at<float>(0, i);
+double* matrixToArray(Mat* m) {
+  double* array = (double*)malloc(m->rows * sizeof(double));
+  for (int i = 0; i < m->rows; i++) {
+    array[i] = m->at<float>(i);
   }
   return array;
 }
 
-/**
-* Calculates the RGB channels histogram. The histogram will be stored in an array of
-* of points, each point has the index imageIndex. The array has three entries,
-* the first entry (i.e the array at index 0) us the red channel histogram, the second entery
-* is the green channel histogram and the third entry is the blue channel histogram.
-*
-* @param str - The path of the image for which the histogram will be calculated
-* @param nBins - The number of subdivision for the intensity histogram
-* @param imageIndex - The index of the given image
-* @return NULL if str is NULL or nBins <= 0 or allocation error occurred,
-*  otherwise a two dimensional array representing the histogram.
-*/
+Mat* pointToMatrix(SPPoint* point) {
+  Mat* mat = new Mat(spPointGetDimension(point), 1, CV_32F);
+  for (int i = 0; i < spPointGetDimension(point); i++) {
+    mat->at<float>(i) =  spPointGetAxisCoor(point, i);
+  }
+  return mat;
+}
+
 SPPoint** spGetRGBHist(const char* str, int imageIndex, int nBins) {
   if (str == NULL || nBins <= 0)
     return NULL;
@@ -43,8 +39,13 @@ SPPoint** spGetRGBHist(const char* str, int imageIndex, int nBins) {
     return NULL;
 
   // Read the image from the file.
-  Mat image = imread(str, CV_LOAD_IMAGE_GRAYSCALE);
-  float range[] = { 0, nBins };
+  Mat image = imread(str, CV_LOAD_IMAGE_COLOR);
+
+  if (image.empty()) {
+    return NULL;
+  }
+
+  float range[] = { 0, 256 };
   const float* histRange = { range };
   int nImages = 1;
 
@@ -58,18 +59,25 @@ SPPoint** spGetRGBHist(const char* str, int imageIndex, int nBins) {
   /// Compute the histograms: 
   /// The results will be store in brg_hists.
   /// The output type of the matrices is CV_32F (float)
-  for (int i = 0; i<N; i++) {
+  for (int i = 0; i < N; i++) {
     //instead of bgr(like the opencv example we use rgb for negeting cognitive dissonance 
     calcHist(&bgr_planes[N - i - 1], nImages, 0, Mat(), rgb_hists[i], 1, &nBins, &histRange);
-    rgbHist[i] = spPointCreate(matrixToArray(&rgb_hists[i], nBins), nBins, imageIndex);
+    //rgb_hists[i].convertTo(rgb_hists[i], CV_64F);
+    rgbHist[i] = spPointCreate(matrixToArray(&rgb_hists[i]), nBins, imageIndex);
   }
   return rgbHist;
 }
 
 double spRGBHistL2Distance(SPPoint** rgbHistA, SPPoint** rgbHistB) {
-  return NULL;
+  if (rgbHistA == NULL || rgbHistB == NULL)
+    return -1;
+  int dim = spPointGetDimension(rgbHistA[0]);
+  double sum = 0;
+  for (int i = 0; i<N; i++) {
+    sum += 0.33*spPointL2SquaredDistance(rgbHistA[i], rgbHistB[i]);
+  }
+  return sum;
 }
-
 SPPoint** spGetSiftDescriptors(const char* str, int imageIndex, int nFeaturesToExtract, int *nFeatures) {
   return NULL;
 }
@@ -80,7 +88,57 @@ int* spBestSIFTL2SquaredDistance(int kClosest, SPPoint* queryFeature,
   return NULL;
 }
 
-int main() {
+int drawRGBHist(SPPoint** rgb_hists, int nBins) {
+  //*****************************************************************
+  // This is not relevant for the Assignment - You can read it for fun
+  //*****************************************************************
+  // Draw the histograms for B, G and R
+  int hist_w = 512;
+  int hist_h = 400;
+  int bin_w = cvRound((double)hist_w / nBins);
 
+  Mat histImage(hist_h, hist_w, CV_8UC3, Scalar(0, 0, 0));
+
+  Mat* hists[N];
+  Scalar scalars[N];
+  scalars[0] = Scalar(255, 0, 0);
+  scalars[1] = Scalar(0, 255, 0);
+  scalars[2] = Scalar(0, 0, 255);
+
+  for (int i = 0; i < N; i++) {
+    hists[i] = pointToMatrix(rgb_hists[N - i - 1]);
+  }
+
+  /// Normalize the result to [ 0, histImage.rows ]
+  for (int i = 0; i < N; i++) {
+    normalize(*hists[i], *hists[i], 0, histImage.rows, NORM_MINMAX, -1, Mat());
+  }
+
+  /// Draw for each channel
+  for (int i = 1; i < nBins; i++) {
+    for (int j = 0; j < N; j++) {
+      line(histImage,
+        Point(bin_w * (i - 1), hist_h - cvRound(hists[j]->at<float>(i - 1))),
+        Point(bin_w * (i), hist_h - cvRound(hists[j]->at<float>(i))),
+        scalars[j], 2, 8, 0);
+    }
+  }
+
+  /// Display
+  namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE);
+  imshow("calcHist Demo", histImage);
+
+  waitKey(0);
+
+  return 0;
+}
+
+int main() {
+  int nBins = 256;
+  SPPoint** histograms = spGetRGBHist("images/img12.png", 1, nBins);
+  if (histograms == NULL) {
+    return -1;
+  }
+  drawRGBHist(histograms, nBins);
   return 0;
 }
