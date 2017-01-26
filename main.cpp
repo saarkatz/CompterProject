6e0e43c94fec1aa012d5883e4e8f99c01a46e9c1
Saar Katz <kats.saar@gmail.com>
2017-01-27 01:54:55 +0200

    Refactored memory failure checks.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,8 @@ extern "C" {
 
 #define ERROR_AND_EXIT(Message) printError((char*)Message);\
                           TERMINATE(1)
+#define CHECK_MEMORY_FAILURE(ARG) if (ARG == NULL) { \
+                                    ERROR_AND_EXIT(MEMORY_FAILURE);}
 #define MEMORY_FAILURE "memory failure"
 #define INVALID_NUMBER_OF_IMAGES "invalid number of images\n"
 #define INVALID_NUMBER_OF_BINS "invalid number of bins\n"
@@ -85,20 +87,15 @@ int main() {
 
 //stage 2
   globalArray = (SPPoint***)malloc(numOfImages * sizeof(SPPoint**));
-  if (globalArray == NULL) {
-    ERROR_AND_EXIT(MEMORY_FAILURE);
-  }
+  CHECK_MEMORY_FAILURE(globalArray);
+
   localArray = (SPPoint***)malloc(numOfImages * sizeof(SPPoint**));
-  if (localArray == NULL) {
-    ERROR_AND_EXIT(MEMORY_FAILURE);
-  }
+  CHECK_MEMORY_FAILURE(localArray);
+
   featureSizes = (int*)calloc(numOfImages , sizeof(int));
-  if (featureSizes == NULL) {
-    ERROR_AND_EXIT(MEMORY_FAILURE);
-  }
+  CHECK_MEMORY_FAILURE(featureSizes);
 
   for (int i = 0; i < numOfImages; i++) {
-    //imageDirectory+imageSuffix+index+imageSuffix
     sprintf(filePath, "%s%s%d%s", imageDirectory, imagePrefix, i, imageSuffix);
     globalArray[i] = spGetRGBHist(filePath, i, numberOfBins);
     localArray[i] = spGetSiftDescriptors(filePath, i, numberOfFeatures, &featureSizes[i]); // This is the same as "features + i"
@@ -110,48 +107,35 @@ int main() {
 
   //stage 4
   totalMatches = (SPImageCounter*)malloc(numOfImages * sizeof(SPImageCounter));
-  if (totalMatches == NULL) {
-    ERROR_AND_EXIT(MEMORY_FAILURE);
-  }
-
+  CHECK_MEMORY_FAILURE(totalMatches);
 
   while (queryPath[0] != '#') {
-   for (int i = 0; i < numOfImages; i++) {
+    for (int i = 0; i < numOfImages; i++) {
       totalMatches[i] = { i, 0 };
     }
 
 
     //initilize queryImageHistogram
     queryImageHistogram = spGetRGBHist(queryPath, 0, numberOfBins);
-    if (queryImageHistogram == NULL) {
-      ERROR_AND_EXIT(MEMORY_FAILURE);
-    }
+    CHECK_MEMORY_FAILURE(queryImageHistogram);
 
     //initilize queryImageFeatures
     queryImageFeatures = spGetSiftDescriptors(queryPath,
       0, numberOfFeatures, &numOfQueryFeatures);
-    if (queryImageFeatures == NULL) {
-      ERROR_AND_EXIT(MEMORY_FAILURE);
-    }
+    CHECK_MEMORY_FAILURE(queryImageFeatures);
 
     resultArray = spBestHistDistance(K_CLOSEST, queryImageHistogram, numOfImages, globalArray);
-    if (resultArray == NULL) {
-      ERROR_AND_EXIT(MEMORY_FAILURE);
-    }
+    CHECK_MEMORY_FAILURE(resultArray);
 
     printKclosest(resultArray, K_CLOSEST, (char*)"global");
     free(resultArray);
-
-
-
 
     //calculate closest local feature for each query feature
     for (int i = 0; i < numOfQueryFeatures; ++i) {
       resultArray = spBestSIFTL2SquaredDistance(K_CLOSEST, queryImageFeatures[i], localArray,
         numOfImages, featureSizes);
-      if (resultArray == NULL) {
-        ERROR_AND_EXIT(MEMORY_FAILURE);
-      }
+      CHECK_MEMORY_FAILURE(resultArray);
+
       //count the images with the closest features
       for (int j = 0; j < K_CLOSEST; ++j) {
         totalMatches[resultArray[j]].count++;
@@ -159,21 +143,16 @@ int main() {
       free(resultArray);
     }
     qsort(totalMatches, numOfImages, sizeof(SPImageCounter), compare_count);
+
     resultArray = (int*)malloc(K_CLOSEST * sizeof(int));
-    if (resultArray == NULL) {
-      ERROR_AND_EXIT(MEMORY_FAILURE);
-    }
+    CHECK_MEMORY_FAILURE(resultArray);
+
     for (int i = 0; i < K_CLOSEST; i++) {
       resultArray[i] = totalMatches[i].index;
     }
 
-
-
-
-
     printKclosest(resultArray, K_CLOSEST, (char*)"local");
     free(resultArray);
-
 
     freeHistogram(queryImageHistogram);
     freeFeatures(queryImageFeatures,numOfQueryFeatures);
@@ -181,8 +160,10 @@ int main() {
     queryImagePrompt();
     scanf("%s", queryPath);
   }
-
-
+  // Here queryImageHistogram and queryImageFeatures should be free so we
+  // dont want to send invalid pointers to terminate or else we will get segfault
+  queryImageHistogram = NULL;
+  queryImageFeatures = NULL;
 
   exitingMsg();
   TERMINATE(0);
