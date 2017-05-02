57c7e099de05da8b454b0470c8d22ff633528833
Ohad Rubin <iohadrubin@gmail.com>
2017-05-02 14:32:19 +0300

    added header for main_aux.h

--- finalProject/main.cpp
+++ finalProject/main.cpp
@@ -1,6 +1,31 @@
-int main(){
+
+#define DEFAULT_FILE "spcbir.config"
+/*spImagesDirectory
+spExtractionMode
+spImagesPrefix
+spImagesSuffix
+spKDTreeSplitMethod
+spKNN
+spLoggerFilename
+spLoggerLevel
+spMinimalGUI
+spNumOfFeatures
+spNumOfImages
+spNumOfSimilarImages
+spPCADimension
+spPCAFilename*/
+int main(int argc, char const *argv[]){
 	SP_CONFIG_MSG msg;
 	SPConfig config = spConfigCreate(filename,&msg);
+	char* filename;
+	if(argc==1){
+		filename=DEFAULT_FILE;
+	}
+	else{
+		if (argc>3){
+			//TODO - handle the fact that we were given too much parameters
+		}
+	}
 	if(msg!=SP_CONFIG_SUCCESS){
 		printConfigError(msg);
 	}
